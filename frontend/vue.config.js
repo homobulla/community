ad4f9c80c6b485cdd26f36c747927beb258ac41f
fxj <fxj@play4u.cn>
2019-08-12 18:54:31 +0800

    fix  webpack路径问题

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/community/" : "./",
+    // publicPath: process.env.NODE_ENV === "production" ? "/community/" : "./",
+    publicPath: "./",
+
     assetsDir: "static",
     productionSourceMap: false,
     // removeAttributeQuotes: false,
